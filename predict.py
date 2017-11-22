a60895399fca07453ef5ee7fa88f43638463a67d
Lei Huang <lh389@cornell.edu>
2017-11-21 22:48:45 -0800

    add module structures

---
+++ predict.py
@@ -0,0 +1,23 @@
+"""
+"""
+
+
+class Predict(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+
+
+    def get_spectrum(self):
+        pass
+
+
+
+def str2predict(s):
+    """
+    """
+    pass
